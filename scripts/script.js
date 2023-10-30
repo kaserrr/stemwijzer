8fe0020c99594c55049336e3c03274e600a08697
kaserrr <99057099@mydavinci.nl>
2023-10-30 13:26:03 +0100

    Basic voteguide working. I've added the nextQuestion method. And multiple functions.

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ class Question {
     constructor(id, text, answer) {
         this.id = id;
         this.text = text;
-        this.answer = null;
+        this.answer = answer;
     }
 
     answered(choice) {
@@ -13,6 +13,9 @@ class Question {
         return `
             <div>
                 <p>${this.text}</p>
+                <button onclick="answerQuestion('agree')">Eens</button>
+                <button onclick="answerQuestion('none')">Geen van beide</button>
+                <button onclick="answerQuestion('disagree')">Oneens</button>
             </div>
         `;
     }
@@ -31,6 +34,46 @@ class VotingGuide {
     showCurrentQuestion() {
         const currentQuestion = this.questions[this.currentQuestionIndex];
         document.getElementById("questions").innerHTML = currentQuestion.generateHTML();
+    }
+
+    nextQuestion() {
+        if (this.currentQuestionIndex < this.questions.length - 1) {
+            this.currentQuestionIndex++;
+            this.showCurrentQuestion();
+        } else {
+            document.getElementById("result").innerHTML = "Finished!";
+        }
+    }
+}
+
+let votingGuide = new VotingGuide();
+
+let questionData = [
+    { id: 1, text: "Moet er meer geld worden besteed aan onderwijs?" },
+    { id: 2, text: "Moet de inkomstenbelasting worden verhoogd?" },
+    { id: 3, text: "Moet de maximale snelheid omhoog?" },
+    { id: 4, text: "Moet Nederland meer vluchtelingen opnemen?" }
+];
+
+questionData.forEach(question => {
+    votingGuide.addQuestion(new Question(question.id, question.text,));
+});
+
+function startVotingGuide() {
+    document.getElementById("start-container").style.display = "none";
+    document.getElementById("questions-container").style.display = "block";
+    votingGuide.showCurrentQuestion();
+}
+
+function answerQuestion(choice) {
+    votingGuide.questions[votingGuide.currentQuestionIndex].answered(choice);
+    votingGuide.nextQuestion();
+}
+
+function backToLastQuestion() {
+    if(votingGuide.currentQuestionIndex > 0) {
+        votingGuide.currentQuestionIndex--;
+        votingGuide.showCurrentQuestion();
     }
 }
 
